6580358378ddabbbb8689b7147865fde6060875a
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-08 22:29:49 +0900

    add : copy fe-w3 sources

---
+++ api.js
@@ -0,0 +1,27 @@
+const api = {
+  getItem : (type, page = 1, count = 5) => {
+    return api.sendRequest(`item?type=${type}&page=${page}&count=${count}`);
+  },
+  getBoxItemLength : () => {
+    return api.sendRequest('length');
+  },
+  sendRequest: (query, method = 'GET') => {
+    return fetch(`http://localhost:3000/${query}`, {
+      method,
+      headers:{
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(res => {
+      if(!res || res.error) {
+        throw new Error(res ? res.error : '서버와 연결이 끊어졌어요.');
+      } else {
+        return res.json();
+      }
+    })
+    .then(json => json)
+    .catch(error => console.log(err));
+  }
+}
+
+module.exports = api;
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const app = require("express")();
+const port = 3000;
+const resFile = require('./response.json');
+const { keyword, best, event, carousel, box } = resFile;
+
+// app.use(express.static('server'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+  next();
+});
+app.get('/item', (req, res) => {
+  const { query : { type, page, count } } = req;
+  res.status(200);
+  if(type === 'best') {
+    res.json(best);
+    return;
+  }
+  if(type === 'event') {
+    res.json(event);
+    return;
+  }
+  if(type === 'carousel') {
+    res.json(carousel);
+    return;
+  }
+  if(!page || !count) {
+    res.json({error: 'page 또는 count 누락'});
+    return;
+  }
+  if(page * count > box.list.length) {
+    res.json({error: '잔여 상품 없음'});
+    return;
+  }
+  res.json({
+    prefix : box.prefix,
+    list : box.list.slice(count * page - count, count * page)
+  });
+});
+
+app.get('/length', (req, res) => {
+  res.status(200);
+  res.json({
+    length : box.list.length
+  });
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server Loaded, http://localhost:${port}`);
+});
