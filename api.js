7a732b57fc2ed3b51a789506d1eba15723f2d616
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-10 22:07:08 +0900

    refactor : api.js
    add constant variables : url, port
    getItem() parameter list -> object
    sendRequest() promise -> async await

--- api.js
+++ api.js
@@ -1,33 +1,31 @@
+
+const PREFIX = 'http://localhost:';
+const PORT = '3000';
+
 const api = {
-  getItem : (type, page = 1, count = 5) => {
-    return api.sendRequest(`item?type=${type}&page=${page}&count=${count}`);
+  getItem : ({ type, page = 1, count = 5 }) => {
+    return api.sendRequest(`/item?type=${type}&page=${page}&count=${count}`);
   },
   getBoxItemLength : () => {
-    return api.sendRequest('length');
+    return api.sendRequest('/length');
   },
   getKeyword: () => {
-    return api.sendRequest('keyword');
+    return api.sendRequest('/keyword');
   },
   getSearchList: async keyword => {
-    return api.sendRequest(`search?word=${keyword}`);
+    return api.sendRequest(`/search?word=${keyword}`);
   },
-  sendRequest: (query, method = 'GET') => {
-    return fetch(`http://localhost:3000/${query}`, {
-      method,
-      headers:{
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(res => {
-      if(!res || res.error) {
-        throw new Error(res ? res.error : '서버와 연결이 끊어졌어요.');
-      } else {
-        return res.json();
+  sendRequest: async (query, method = 'GET') => {
+    try {
+      const data = await fetch(`${PREFIX}${PORT}${query}`, { method });
+      if(!data || data.error) {
+        throw new Error(data && data.error || '서버와 연결이 끊어졌어요.');
       }
-    })
-    .then(json => json)
-    .catch(error => console.log(error));
-  },
+      return await data.json();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = api;
