22656832e04cee8f73bf30aee86396ae59a3e70c
kowoohyuk <kowoohyuk91@gmail.com>
2021-03-10 21:33:08 +0900

    refactor : server.js

--- server/server.js
+++ server/server.js
@@ -1,48 +1,33 @@
 const app = require("express")();
-const port = 3000;
-const resFile = require('./response.json');
 const fetch = require('node-fetch');
-const { keyword, best, event, carousel, box } = resFile;
+const { keyword, best, event, carousel, box } = require('./response.json');
+
+const PORT = 3000;
+const AMAZONPATH = 'https://completion.amazon.com/api/2017/suggestions?mid=ATVPDKIKX0DER&alias=aps&suggestion-type=KEYWORD&prefix=';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, X-Requested-With");
   next();
 });
+
 app.get('/item', (req, res) => {
   const { query : { type, page, count } } = req;
   res.status(200);
-  if(type === 'best') {
-    res.json(best);
-    return;
+
+  switch(type) {
+    case 'best'    : res.json(best); break;
+    case 'event'   : res.json(event); break;
+    case 'carousel': res.json(carousel); break;
+    case 'best'    : res.json(best); break;
+    case 'box'     : res.json(getBoxList({page, count})); break;
+    default : res.status(400); break;
   }
-  if(type === 'event') {
-    res.json(event);
-    return;
-  }
-  if(type === 'carousel') {
-    res.json(carousel);
-    return;
-  }
-  if(!page || !count) {
-    res.json({error: 'page 또는 count 누락'});
-    return;
-  }
-  if(page * count > box.list.length) {
-    res.json({error: '잔여 상품 없음'});
-    return;
-  }
-  res.json({
-    prefix : box.prefix,
-    list : box.list.slice(count * page - count, count * page)
-  });
 });
 
 app.get('/length', (req, res) => {
   res.status(200);
-  res.json({
-    length : box.list.length
-  });
+  res.json({ length : box.list.length });
   res.end();
 });
 
@@ -54,10 +39,9 @@ app.get('/keyword', (req, res) => {
 
 app.get('/search', async (req, res) => {
   const { query : { word } } = req;
-  const PATH = 'https://completion.amazon.com/api/2017/suggestions?mid=ATVPDKIKX0DER&alias=aps&suggestion-type=KEYWORD&prefix=';
   try {
     res.status(200);
-    const data = await fetch(PATH + word);
+    const data = await fetch(AMAZONPATH + word);
     const json = await data.json();
     const result = json.suggestions.reduce((acc, cur) => [...acc, cur.value], []);
     res.json({'list' : result});
@@ -69,6 +53,19 @@ app.get('/search', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server Loaded, http://localhost:${port}`);
+const getBoxList = ({ page, count }) => {
+  if(!page || !count) {
+    return {error: 'page 또는 count 누락'};
+  }
+  if(page * count > box.list.length) {
+    return {error: '잔여 상품 없음'};
+  }
+  return {
+    prefix : box.prefix,
+    list : box.list.slice(count * page - count, count * page)
+  };
+};
+
+app.listen(PORT, () => {
+  console.log(`Server Loaded, http://localhost:${PORT}`);
 });
